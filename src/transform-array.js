a173d0c3206259b59d12b44db4f1e0f3b316f33b
Blshop <envworking@gmail.com>
2025-11-08 17:15:09 +0300

    feat: task completed

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,43 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if (!(arr instanceof Array)) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!')
+  }
+  let transformed = []
+  let discarded = false
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == '--double-next') {
+      if (i != arr.length - 1) {
+        transformed.push(arr[i + 1])
+        transformed.push(arr[i + 1])
+        i++
+      }
+    }
+    else if (arr[i] == '--double-prev') {
+      if (transformed.length > 0 && discarded == false) {
+        transformed.push(transformed[transformed.length - 1])
+      }
+      else {
+        discarded = false
+      }
+    }
+    else if (arr[i] == '--discard-next') {
+      discarded = true
+      i++
+    }
+    else if (arr[i] == '--discard-prev') {
+      if (!discarded) {
+        transformed.pop()
+      }
+    }
+    else {
+      transformed.push(arr[i])
+      discarded = false
+    }
+  }
+  return transformed
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,46 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  encrypt(message, key) {
+    if (!message || !key) { throw new Error("Incorrect arguments!") }
+    let result = []
+    let j = 0
+    message = message.toUpperCase()
+    key = key.toUpperCase()
+    for (let i = 0; i < message.length; i++) {
+      if (message.charAt(i).match(/[a-z]/i)) {
+        let char_code = ((message.charCodeAt(i) + key.charCodeAt(j % key.length) - 2 * 65) % 26) + 65
+        j++
+        result.push(String.fromCharCode(char_code))
+      }
+      else {
+        result.push(message[i])
+      }
+    }
+    return this.isDirect ? result.join('') : result.reverse().join('');
+    // remove line with error and write your code here
+  }
+  decrypt(message, key) {
+    if (!message || !key) { throw new Error("Incorrect arguments!") }
+    let result = []
+    let j = 0
+    message = message.toUpperCase()
+    key = key.toUpperCase()
+    for (let i = 0; i < message.length; i++) {
+      if (message.charAt(i).match(/[a-z]/i)) {
+        let char_code = ((message.charCodeAt(i) - key.charCodeAt(j % key.length) + 26) % 26) + 65
+        j++
+        result.push(String.fromCharCode(char_code))
+      }
+      else {
+        result.push(message[i])
+      }
+    }
+    return this.isDirect ? result.join('') : result.reverse().join('');
+    // remove line with error and write your code here
   }
 }
 
